f1b092ada9e18a91642d46ca0d47dceb73324298
Alexander Olofsson <alexander.olofsson@liu.se>
2025-08-11 12:18:29 +0200

    Another Rubocop complaint

--- app/models/concerns/foreman_liu_patches/dns_handling.rb
+++ app/models/concerns/foreman_liu_patches/dns_handling.rb
@@ -17,9 +17,9 @@ module ForemanLiuPatches
       # If the subnet DHCP server doesn't want hostnames, always store it as an IPv4 address
       unless subnet.dhcp.has_capability?(:DHCP, :dhcp_filename_hostname)
         res = Resolv::DNS
-          .open { |dns| dns.getaddresses(boot_server_or_proxy_hostname) }
-          .select { |addr| addr.is_a? Resolv::IPv4 }
-          .first
+              .open { |dns| dns.getaddresses(boot_server_or_proxy_hostname) }
+              .select { |addr| addr.is_a? Resolv::IPv4 }
+              .first
         return res.to_s if res
       end
 
